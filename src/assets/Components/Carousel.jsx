742f2918c400e6798d656c047045e1f17035e86e
Santiago <santiagonlaghi@hotmail.com>
2023-08-30 01:21:45 -0300

    M05-Terminado

--- src/assets/Components/Carousel.jsx
+++ src/assets/Components/Carousel.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import ArrowLeft from './ArrowLeft'
 import ArrowRigth from './ArrowRigth'
+import axios from 'axios'
 
 export default function Carousel() {
+  let [categories, setCategories] = useState([])
+  let [counter, setCounter] = useState(0)
+  function getData() {
+    axios('https://minga-back-vyqy.onrender.com/categories')
+      .then(res => setCategories(res.data.categories))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getData(),
+      []
+  })
+
+  let next = () => (counter !== categories.length - 1) ? setCounter(counter + 1) : setCounter(0)
+  let prev = () => (counter !== 0) ? setCounter(counter - 1) : setCounter(categories.length - 1)
+
   return (
+
     <div className="max-md:hidden md: my-5 w-10/12 h-56 bg-gradient-to-b from-orange-600 to-orange-500 flex items-center px-5 justify-between md:h-1/4">
-          <ArrowLeft/>
-          <img className="h-40 mb-10" src="./images/image1.png" alt="" />
-          <img className="h-40 mb-8" src="./images/image2.png" alt="" />
-          <div className='md:w-48 lg:w-96'>
-            <h4 className='text-white md:text-center lg:text-left'>Shonen:</h4>
-            <p className='text-white text-xs md:text-center lg:text-left'>Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
-          </div>
-          <ArrowRigth/>
+      <ArrowLeft onClick={prev} />
+      <img className="h-40 mb-10" src={categories[counter]?.character_photo} alt="" />
+      <img className="h-40 mb-8" src={categories[counter]?.cover_photo} alt="" />
+      <div className='md:w-48 lg:w-96'>
+        <h4 className='text-white md:text-center lg:text-left'>{categories[counter]?.name}</h4>
+        <p className='text-white text-xs md:text-center lg:text-left'>{categories[counter]?.description}</p>
+      </div>
+      <ArrowRigth onClick={next} />
     </div>
   )
 }
--- src/assets/Layout/Footer.jsx
+++ src/assets/Layout/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='h-48 md:flex flex-col'>
-    <img className="w-full" src="./images/footer.png" alt="" />
-    <div className='flex flex-col items-center pt-2 gap-2 md:flex-row md:justify-around self-center md:w-5/6'>
-      <p className='text-md cursor-pointer'>Home</p>
-      <p className='text-md cursor-pointer'>Mangas</p>
-      <div className='flex md:w-48'>
-        <p className="w-20 h-10 text-orange-600 cursor-pointer">Minga</p>
-        <p className='text-black'>雪</p>
+      <img className="w-full" src="./images/footer.png" alt="" />
+      <div className='flex flex-col  pt-2 gap-2 md:flex-row md:justify-around self-center md:w-5/6 items-center md:items-baseline' >
+        <p className='w-[64.86px] h-6 text-neutral-800 text-base font-normal'>Home</p>
+        <p className='w-[87.83px] h-6 text-neutral-800 text-base font-normal'>Mangas</p>
+        <div className='flex md:w-48'>
+          <p className="w-[126px] text-orange-500 text-4xl font-medium tracking-widest ">Minga</p>
+          <p className='w-[43px] text-black-50 text-[40px] font-extrabold tracking-[16px]'>雪</p>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <img src="./images/Social.svg" alt="" />
+          <button className=' h-12 text-white bg-gradient-to-r from-orange-600 to-orange-500 rounded-md mb-4'>Donate ♡</button>
+        </div>
       </div>
-      <div className='flex flex-col gap-2'>
-        <img src="./images/Social.svg" alt="" />
-        <button className='text-white bg-gradient-to-r from-orange-600 to-orange-500 rounded-md'>Donate ♡</button>
-      </div>
-    </div>
-  </footer>
+    </footer>
   )
 }
